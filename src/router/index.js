1ac132249d9160e496c5b5f3454f223ed65460d6
RifkyRhml <166179086+RifkyRhml@users.noreply.github.com>
2024-06-17 14:34:55 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const router = createRouter({
       component: MainLayout,
       children: [
         {
-          path: "/Todo",
+          path: "/",
           component: Todo,
         },
         {
